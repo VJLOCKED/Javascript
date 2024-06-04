773e8d3e04eb1f7e53b8eba07d871cc8c868e196
VJLOCKED <vishwanathjumanal412@gmail.com>
2024-06-04 10:52:48 +0530

    new .js file added

---
+++ Incre_decrement.js
@@ -0,0 +1,9 @@
+let noOfQuantity = 0
+let operation = "increment"
+if (operation === "increment") {
+    noOfQuantity++
+}else {
+    noOfQuantity--
+}
+
+console.log("noOfQuantity======> " + noOfQuantity)
