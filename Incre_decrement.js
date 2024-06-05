effecb6d48c07aeb41bd4c5cafdc5e2a1b834da9
VJLOCKED <vishwanathjumanal412@gmail.com>
2024-06-05 09:40:06 +0530

    modified code

--- Incre_decrement.js
+++ Incre_decrement.js
@@ -7,3 +7,24 @@ if (operation === "increment") {
 }
 
 console.log("noOfQuantity======> " + noOfQuantity)
+
+let num= 10;
+num++;
+console.log(num)
+
+let num1=5 ;
+let num2=10;
+console.log(num1/num2)
+
+let num4=5 ;
+let num3=10;
+console.log(num1/num2)
+
+let isSubscribed=1;
+if(isSubscribed)
+    { console.log("no condition======>")}
+
+console.log (typeof 0 )
+
+if(1 == '1')
+    { console.log("hii") }
